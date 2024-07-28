d4d76e97600cb857906bd5bbebd15cf62d9ae21c
QSeeng <itnotfunanymore@gmail.com>
2024-07-28 21:57:23 +0300

    Fix errors [3]

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -4,15 +4,14 @@ import {
 } from '../index.js';
 
 const greatestCommonDivisor = (num1, num2) => {
-  while (num1 !== 0 && num2 !== 0) {
-    if (num1 > num2) {
-      num1 %= num2;
-    } else {
-      num2 %= num1;
+  const minNum = num1 > num2 ? num2 : num1;
+  let gcd = 1;
+  for (let i = 2; i <= minNum; i += 1) {
+    if (num1 % i === 0 && num2 % i === 0) {
+      gcd = i;
     }
   }
-  const result = num1 + num2;
-  return result;
+  return gcd;
 };
 
 const playGreatestCommonDivisor = () => {
